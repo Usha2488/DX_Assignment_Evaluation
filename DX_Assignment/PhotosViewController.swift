924f41729b5d1cb449292a5316f264e8db7eeada
GitUser24 <ushki.c@gmail.com>
2019-11-11 20:07:14 +0530

    Deleted Storyboard and added new view controller
    Deleted storyboard as per the guidelines. Added new view controller to create table view

---
+++ DX_Assignment/PhotosViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PhotosViewController.swift
+//  DX_Assignment
+//
+//  Created by Usha Chawla on 11/11/19.
+//  Copyright © 2019 Usha Chawla. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+  
+
+}
