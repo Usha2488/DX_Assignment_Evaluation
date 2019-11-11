e59a6a1dd055ae3753db406c1618bb2d2a29486e
GitUser24 <ushki.c@gmail.com>
2019-11-11 20:47:22 +0530

    Added base structure of UITableView
    Added basic structure of UITableview to the code. Added constraints for layout changes.

--- DX_Assignment/AppDelegate.swift
+++ DX_Assignment/AppDelegate.swift
@@ -16,10 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
-    let window = UIWindow(frame: UIScreen.main.bounds)
-    window.makeKeyAndVisible()
-    window.rootViewController = PhotosViewController()
-  
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.makeKeyAndVisible()
+    window?.rootViewController = UINavigationController(rootViewController: PhotosViewController())
     return true
   }
 
--- DX_Assignment/PhotosViewController.swift
+++ DX_Assignment/PhotosViewController.swift
@@ -8,14 +8,38 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
-
+class PhotosViewController: UIViewController, UITableViewDataSource {
+    
+    let photosTableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.title = "Assignment"
+        
+        self.view.addSubview(photosTableView)
+        photosTableView.translatesAutoresizingMaskIntoConstraints = false
+        setTableViewLayout()
+        
+        photosTableView.dataSource = self
+        photosTableView.register(UITableViewCell.self, forCellReuseIdentifier: "photoCell")
     }
     
-
-  
-
+    func setTableViewLayout() {
+        photosTableView.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor).isActive = true
+        photosTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        photosTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        photosTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "photoCell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
+    }
 }
+
