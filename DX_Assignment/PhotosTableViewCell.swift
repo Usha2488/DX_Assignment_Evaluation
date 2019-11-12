e30c083b2de9986e084a5379ba7a11406b8fd59e
GitUser24 <ushki.c@gmail.com>
2019-11-12 20:49:13 +0530

    Added custom UITableViewCell class to create custom cell for the tableview. Modified cellForRowAtIndexPath method in PhotosViewController to use custom cell.

---
+++ DX_Assignment/PhotosTableViewCell.swift
@@ -0,0 +1,76 @@
+//
+//  PhotosTableViewCell.swift
+//  DX_Assignment
+//
+//  Created by Usha Chawla on 12/11/19.
+//  Copyright © 2019 Usha Chawla. All rights reserved.
+//
+
+import UIKit
+
+class PhotosTableViewCell: UITableViewCell {
+    
+    lazy var photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .clear
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.backgroundColor = .clear
+        label.textColor = .black
+        label.font = label.font.withSize(15.0)
+        return label
+    }()
+    
+    lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.backgroundColor = .clear
+        label.textColor = .black
+        label.font = label.font.withSize(13.0)
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        layoutViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layoutViews()
+    }
+    
+    func layoutViews() {
+        contentView.addSubview(photoImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(descriptionLabel)
+        
+        NSLayoutConstraint.activate([
+            photoImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
+            photoImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 1),
+            photoImageView.widthAnchor.constraint(equalToConstant: 70),
+            photoImageView.heightAnchor.constraint(equalTo: photoImageView.widthAnchor),
+            photoImageView.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: -5),
+            
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 1),
+            titleLabel.leadingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: 8),
+            titleLabel.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor, constant: -10),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 2),
+            descriptionLabel.heightAnchor.constraint(lessThanOrEqualToConstant: 200),
+            descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5),
+            descriptionLabel.leadingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: 8),
+            descriptionLabel.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor, constant: -5)
+            ])
+        
+    }
+    
+}
--- DX_Assignment/PhotosViewController.swift
+++ DX_Assignment/PhotosViewController.swift
@@ -9,107 +9,102 @@
 import UIKit
 
 class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-  
-  let photosTableView = UITableView()
-  let webServiceShared = WebServiceManager.webServiceManagerSharedInstance
-  var screenTitle : String?
-  var rowsArray =  [[String : Any]]()
-  
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    getJsonDataFromApi()
-    self.view.addSubview(photosTableView)
-    photosTableView.translatesAutoresizingMaskIntoConstraints = false
-    setTableViewLayout()
     
-    photosTableView.delegate = self
-    photosTableView.dataSource = self
-    photosTableView.register(UITableViewCell.self, forCellReuseIdentifier: "photoCell")
-  }
-  
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    self.navigationItem.title = screenTitle
-  }
-  
-  func setTableViewLayout() {
-    photosTableView.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor).isActive = true
-    photosTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-    photosTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-    photosTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-  }
-  
-  func getJsonDataFromApi() {
-    webServiceShared.getJsonData { (error, responseData) in
-      guard error == nil else{
-        print(error!)
-        return
-      }
-      print(responseData)
-      self.parseJsonResponse(jsonData: responseData)
-    }
-  }
-  
-  func parseJsonResponse(jsonData : [String : Any]) {
-    screenTitle = jsonData["title"] as? String
-    rowsArray = jsonData["rows"] as! [[String : Any]]
+    let photosTableView = UITableView()
+    let webServiceShared = WebServiceManager.webServiceManagerSharedInstance
+    var screenTitle : String?
+    var rowsArray =  [[String : Any]]()
     
-    DispatchQueue.main.async {
-      self.photosTableView.reloadData()
-    }
-  }
-  
-  func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-    return UITableView.automaticDimension
-  }
-  
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return rowsArray.count
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell: UITableViewCell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "photoCell")
- 
-    let rowDictionary = rowsArray[indexPath.row]
-    if !(rowDictionary["title"] is NSNull){
-      cell.textLabel?.text = rowDictionary["title"] as? String
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getJsonDataFromApi()
+        self.view.addSubview(photosTableView)
+        photosTableView.translatesAutoresizingMaskIntoConstraints = false
+        setTableViewLayout()
+        
+        photosTableView.delegate = self
+        photosTableView.dataSource = self
+        photosTableView.register(PhotosTableViewCell.self, forCellReuseIdentifier: "photoCell")
     }
     
-    if !(rowDictionary["description"] is NSNull) {
-      cell.detailTextLabel?.lineBreakMode = .byWordWrapping
-      cell.detailTextLabel?.numberOfLines = 0
-      let detailString = rowDictionary["description"] as! String
-      cell.detailTextLabel?.text = detailString
+    func setTableViewLayout() {
+        photosTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        photosTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        photosTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        photosTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
     
-    if !(rowDictionary["imageHref"] is NSNull) {
-      webServiceShared.getImageFromUrl(urlString: (rowDictionary["imageHref"] as! String)) { (error, imageData) in
-        guard error == nil else {
-          print(error!)
-          return
+    func getJsonDataFromApi() {
+        webServiceShared.getJsonData { (error, responseData) in
+            guard error == nil else{
+                print(error!)
+                return
+            }
+            print(responseData)
+            self.parseJsonResponse(jsonData: responseData)
         }
+    }
+    
+    func parseJsonResponse(jsonData : [String : Any]) {
+        screenTitle = jsonData["title"] as? String
+        rowsArray = jsonData["rows"] as! [[String : Any]]
+        
         DispatchQueue.main.async {
-          if let rowImage = UIImage.init(data: imageData!){
-            cell.imageView?.image = rowImage
-          } else {
-            print("Image data conversion error")
-            cell.imageView?.image = UIImage(named: "no-image")
-          }
-          cell.setNeedsLayout()
+            self.navigationItem.title = self.screenTitle
+            self.photosTableView.reloadData()
         }
-      }
     }
-    else
-    {
-      DispatchQueue.main.async {
-        cell.imageView?.image = UIImage(named: "no-image")
-        cell.setNeedsLayout()
-      }
+   
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
-    return cell
-  }
-  
-  
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rowsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: PhotosTableViewCell = PhotosTableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "photoCell")
+        
+        let rowDictionary = rowsArray[indexPath.row]
+        if !(rowDictionary["title"] is NSNull){
+            cell.titleLabel.text = rowDictionary["title"] as? String
+        }
+        
+        if !(rowDictionary["description"] is NSNull) {
+            let detailString = rowDictionary["description"] as! String
+            cell.descriptionLabel.text = detailString
+        }
+        
+        if !(rowDictionary["imageHref"] is NSNull) {
+            webServiceShared.getImageFromUrl(urlString: (rowDictionary["imageHref"] as! String)) { (error, imageData) in
+                guard error == nil else {
+                    print(error!)
+                    return
+                }
+                DispatchQueue.main.async {
+                    if let rowImage = UIImage.init(data: imageData!){
+                        cell.photoImageView.image = rowImage
+                    } else {
+                        print("Image data conversion error")
+                        cell.photoImageView.image = UIImage(named: "no-image")
+                    }
+                    cell.setNeedsLayout()
+                }
+            }
+        }
+        else
+        {
+            DispatchQueue.main.async {
+                cell.photoImageView.image = UIImage(named: "no-image")
+                cell.setNeedsLayout()
+            }
+        }
+        return cell
+    }
+    
+    
 }
 
